c723dcc9f4c0a8327f968581bdfb7153eff6537a
RajeshKhandare <rajeshkhandare788@gmail.com>
2026-09-18 17:32:18 +0530

    chore: centralize provisional site identity

--- app/robots.ts
+++ app/robots.ts
@@ -1,3 +1,6 @@
 import { MetadataRoute } from 'next';
-const SITE_URL = 'https://thetoolgenie.com';
-export default function robots():MetadataRoute.Robots{return{rules:{userAgent:'*',allow:'/'},sitemap:SITE_URL+'/sitemap.xml'};}
+import { SITE_URL } from '@/config/site';
+
+export default function robots():MetadataRoute.Robots{
+  return{rules:{userAgent:'*',allow:'/'},sitemap:SITE_URL+'/sitemap.xml'};
+}
