b5e1eff84833b63427cb35f615e9f178637ac09e
RajeshKhandare <rajeshkhandare788@gmail.com>
2026-09-22 19:52:56 +0530

    revert unstable Cloudflare locale route flags

--- app/[locale]/tools/page.tsx
+++ app/[locale]/tools/page.tsx
@@ -7,9 +7,6 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 
-export const dynamic = 'force-static';
-export const dynamicParams = true;
-
 export function generateStaticParams() {
   return LOCALES.filter((locale) => locale.code !== 'en').map((locale) => ({ locale: locale.code }));
 }
