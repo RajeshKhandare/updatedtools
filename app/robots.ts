092493250d422a3005ab5704710ad21920704f81
RajeshKhandare <rajeshkhandare788@gmail.com>
2026-09-22 22:50:30 +0530

    fix static export metadata routes for Cloudflare

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { SITE_URL, SITE_URL_CONFIGURED } from '@/config/site';
 
+export const dynamic = 'force-static';
+
 export default function robots():MetadataRoute.Robots{
   return SITE_URL_CONFIGURED
     ? {rules:{userAgent:'*',allow:'/'},sitemap:SITE_URL+'/sitemap.xml'}
