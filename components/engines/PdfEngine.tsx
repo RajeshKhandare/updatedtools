b513eb98b5566e52b4f6939b23c9af4abedb0445
RajeshKhandare <rajeshkhandare788@gmail.com>
2026-09-23 13:56:06 +0530

    use PDF.js worker for PDF rendering tools

--- components/engines/PdfEngine.tsx
+++ components/engines/PdfEngine.tsx
@@ -66,13 +66,15 @@ async function renderPdf(
     'pdfjs-dist/legacy/build/pdf.mjs'
   );
 
+  pdfjs.GlobalWorkerOptions.workerSrc =
+    '/pdf.worker.min.mjs';
+
   const data = new Uint8Array(
     await file.arrayBuffer()
   );
 
   const doc = await pdfjs.getDocument({
     data,
-    disableWorker: true,
   }).promise;
 
   const pages: {
